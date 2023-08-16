8e338f9189776607ebfac548437568c73b6ad685
--global <felipe.escorciosousa@gmail.com>
2023-08-16 12:44:52 -0300

    ref: adds date filter logics to consulta dolar service

--- consulta-dolar/src/app/service/consulta-dolar-service/consulta-dolar-service.service.ts
+++ consulta-dolar/src/app/service/consulta-dolar-service/consulta-dolar-service.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { distinctUntilChanged, retry } from 'rxjs/operators'
 
 import CotacaoDolar from 'src/app/model/cotacao-dolar.interface';
+import DataCotacao from './interface/data-cotacao.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,20 @@ export class ConsultaDolarService {
 
   private readonly BASE_URL: String = 'localhost:8080/';
 
-  private $consultaDolarSubject: BehaviorSubject<CotacaoDolar> = new BehaviorSubject<CotacaoDolar>({});
+  private $consultaDolarSubject: BehaviorSubject<CotacaoDolar> = new BehaviorSubject<CotacaoDolar>({
+    dataCotacao: '',
+    cotacaoCompra: 0,
+    cotacaoVenda: 0,
+    dataHoraCotacao: ''
+  });
+
+  private dataCotacao: DataCotacao = {
+    dia: '',
+    mes: '',
+    ano: ''
+  }
+
+  private $dataCotacaoSubject: BehaviorSubject<DataCotacao> = new BehaviorSubject<DataCotacao>(this.dataCotacao);
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,15 +34,34 @@ export class ConsultaDolarService {
     return this.$consultaDolarSubject.asObservable();
   }
 
-  public consultaDolarDia(cotacaoDia: String): void {
+  get getUltimaDataCotacao(): Observable<DataCotacao> {
+    return this.$dataCotacaoSubject.asObservable();
+  }
+
+  private get cotacaoDia(): String {
+    return `'${this.formatNumber(this.dataCotacao.mes)}-${this.formatNumber(this.dataCotacao.dia)}-${this.dataCotacao.ano}'`;
+  }
+
+  private formatNumber(num: String) {
+    return num.length === 1 ? '0' + num : num;
+  }
+
+  public consultaDolarDia(dia: String, mes: String, ano: String): void {
+    this.dataCotacao.dia = dia;
+    this.dataCotacao.mes = mes;
+    this.dataCotacao.ano = ano;
+
     this.httpClient
-      .get<CotacaoDolar>(`${this.BASE_URL}consultaDolarDia?cotacaoDia=${cotacaoDia}`)
+      .get<CotacaoDolar>(`${this.BASE_URL}consultaDolarDia?cotacaoDia=${this.cotacaoDia}`)
       .pipe(
         distinctUntilChanged(),
         retry(3)
       )
       .subscribe(
-        (result: CotacaoDolar) => this.$consultaDolarSubject.next(result),
+        (result: CotacaoDolar) => {
+          this.$consultaDolarSubject.next(result);
+          this.$dataCotacaoSubject.next(this.dataCotacao);
+        },
         (err: HttpErrorResponse) => {
           console.error(err);
           this.$consultaDolarSubject.error(err);
